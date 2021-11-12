e02ead190618163eae1e5a4afb9143ea5b628ef0
Vipul Soni35 <visoni35@in.ibm.com>
2021-11-12 18:45:57 +0530

    broken homepage into smaller components, finished styling menu-items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+function HomePage()
+{
+    return(
+  <div className='homepage'>
+     <Directory/>
+     </div>        
+    );
+}
+export default HomePage;
